756a91bde7b2a95bd054d15e89e790e817e9750b
David Molina <davidmolina2810@gmail.com>
2026-09-21 23:44:51 -0500

    add solution file

--- src/Relay.Api/Program.cs
+++ src/Relay.Api/Program.cs
@@ -1,3 +1,5 @@
+using Relay.Modules.Tenancy;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddTenancyModule();
+builder.Services.AddTenancyModule(builder.Configuration);
 
 var app = builder.Build();
 
